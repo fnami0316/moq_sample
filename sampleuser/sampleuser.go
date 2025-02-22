02834908123c050805f9b8a3a2b5353238d494e7
fnami0316 <fnami0316@gmail.com>
2025-02-22 17:39:14 +0900

    TimeOfDay の条件を変更

--- sampleuser/sampleuser.go
+++ sampleuser/sampleuser.go
@@ -10,11 +10,11 @@ type SampleUser struct {
 func (u *SampleUser)TimeOfDay() string {
 	hour := u.Now().Hour()
 
-	if 5 < hour && hour <= 12 {
+	if 5 <= hour && hour < 12 {
 		return "morning"
-	} else if 12 < hour && hour <= 17 {
+	} else if 12 <= hour && hour < 17 {
 		return "afternoon"
-	} else if 17 < hour && hour <= 19 {
+	} else if 17 <= hour && hour < 19 {
 		return "evening"
 	} else {
 		return "night"
